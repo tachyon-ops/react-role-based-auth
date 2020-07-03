16c15b372dff6e7eb0d1d7c222ef7df05ea87882
Nuno Ribeiro <nmpribeiro@gmail.com>
2020-07-03 17:13:42 +0200

    Fixes exports

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-import { Lib } from "./lib";
+import { AuthCallback, BrowserRefresh, SecuredRoute } from './lib/authServices';
+import { AuthContextType } from './lib/roles-based-auth/context';
+import {
+    Can,
+    AuthContext,
+    InitialUserType,
+    initialAuthContext,
+    BaseRoles,
+    StaticRulesType,
+    DynamicRulesType,
+    RulesInterface,
+} from './lib/roles-based-auth';
 
-export { Lib };
+export {
+    AuthCallback,
+    BrowserRefresh,
+    SecuredRoute,
+    Can,
+    AuthContext,
+    AuthContextType,
+    InitialUserType,
+    initialAuthContext,
+    BaseRoles,
+    StaticRulesType,
+    DynamicRulesType,
+    RulesInterface,
+};
