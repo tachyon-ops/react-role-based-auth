3706666aee407878dc193126067731da2d32b334
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-05-25 08:06:07 +0200

    Updates

---
+++ precache-manifest.6b41f4c4cf67c151d3b19f347c7856f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d81ce06e22dba31d274eb80256b55a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70526b52a8b9e3b7f0f8",
+    "url": "./static/css/main.5d945ead.chunk.css"
+  },
+  {
+    "revision": "d1770c8c6259d0840012",
+    "url": "./static/js/2.37e8c0ae.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.37e8c0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70526b52a8b9e3b7f0f8",
+    "url": "./static/js/main.f75c085a.chunk.js"
+  },
+  {
+    "revision": "537b42ca1d4e64681091",
+    "url": "./static/js/runtime-main.4b8dfd1f.js"
+  },
+  {
+    "revision": "a02392f5ea4ef5b2e89adf7c992f9512",
+    "url": "./static/media/udilia-logo.a02392f5.svg"
+  }
+]);
